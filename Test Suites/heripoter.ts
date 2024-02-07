f8cabe2bc9a6413d705ab066cd0bc69fc6243c6d
piawaitrisulo <piawaitrisulo@localhost>
2024-02-07 11:23:11 +0700

    pusing cuy

---
+++ Test Suites/heripoter.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>heripoter</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>210dc257-6d51-4300-aecb-752de4620781</testSuiteGuid>
+   <testCaseLink>
+      <guid>c14c2161-4e59-4bfa-8e42-35667a28522c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/heripoter</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
